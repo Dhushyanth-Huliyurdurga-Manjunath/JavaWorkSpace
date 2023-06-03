e76fb26e5578c5b0e95f66f634340f20dfdeef9d
Dhushyanth-h-m <93438767+Dhushyanth-h-m@users.noreply.github.com>
2023-06-03 14:07:31 -0500

    HelloWorld

---
+++ HelloWord.java
@@ -0,0 +1,7 @@
+public class HelloWord {
+    //class name has to be same as the file name
+    public static void main(String[] args){
+        //Prints Hello World on the screen
+        System.out.println("Hello World!");
+    }
+}
