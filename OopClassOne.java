6a434cd80fb2c8d35e693af751c0a9d6f5218646
Dhushyanth-Huliyurdurga-Manjunath <dxh8160@mavs.uta.edu>
2023-06-08 19:54:28 -0500

    OOP with the use of 2 different classes and methods

---
+++ OopClassOne.java
@@ -0,0 +1,9 @@
+public class OopClassOne {
+    public static void sayHello(){ //method with no return type (VOID)
+        System.out.println("Hello from a different class!");
+    }
+    static final int year = 2023; //An integer variable declared as final so that it cannot be modified
+    public static int whatYearIsIt(){  //method with integer return type
+        return year;
+    }
+}
