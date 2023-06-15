f151e56a0b8a175715d96ab2be808f7be7e831a8
Dhushyanth-Huliyurdurga-Manjunath <125176193+Dhushyanth-Huliyurdurga-Manjunath@users.noreply.github.com>
2023-06-15 18:16:52 +0000

    Chain constructors

--- ChainConstructors.java
+++ ChainConstructors.java
@@ -12,15 +12,26 @@ class City{
     }
 }
 
-public class ChainConstructors extends City{
+public class ChainConstructors extends City{ //Inheritance from class "City"
+    static ChainConstructors instance = null;
+    public int a = 0;
+    ChainConstructors(){}
     ChainConstructors(String name, String areaCode){ //Constructor
         super(name, areaCode); //Calls the base class's constructor "City(String name, String code)"
         System.out.println("End-of-Code");
     }
-    static public ChainConstructors getInstance(){
-        
+    static public ChainConstructors getInstance(){ //singleton constructor: at any time there can only be one object for this class
+        if(instance == null){
+            instance = new ChainConstructors();
+        }
+        return instance;
     }
     public static void main(String[] args){
         new ChainConstructors("Arlington", "817");
+        ChainConstructors object1 = ChainConstructors.getInstance();
+        ChainConstructors object2 = ChainConstructors.getInstance();
+        object1.a += 10; //object 1's attribute modification 
+        System.out.println("Accessing attribute from \"Object1\": "+object1.a);
+        System.out.println("Accessing attribute from \"Object2\": "+object2.a); //even object 2 accesses the same value as object 1 
     }
 }
