09c2f31b2de9133d83626087a05887727bc66f17
Dhushyanth-Huliyurdurga-Manjunath <dxh8160@mavs.uta.edu>
2023-06-14 20:37:36 -0500

    Hello world in a GUI application

--- HelloWord.java
+++ HelloWord.java
@@ -1,7 +1,13 @@
+import javax.swing.*; //swing package to provide GUI components to the application
 public class HelloWord {
     //class name has to be same as the file name
     public static void main(String[] args){
         //Prints Hello World on the screen
         System.out.println("Hello World!");
+        JFrame frame = new JFrame( "Hello, Java!" ); //Represents a window in the application
+        JLabel label = new JLabel("Hello, Java!", JLabel.CENTER ); //Text label contained in the container
+        frame.add(label);
+        frame.setSize( 1440, 900 );
+        frame.setVisible( true ); //window visibility
     }
 }
